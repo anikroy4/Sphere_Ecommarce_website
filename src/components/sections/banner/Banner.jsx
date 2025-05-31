d79b6035380fba2543526df57cae82d5acaf0706
Anik Roy <anikroy.uiu@gmail.com>
2025-05-31 16:23:26 +0600

    banner added

---
+++ src/components/sections/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='max-w-[1440px] mx-auto'>
+        <div className=' w-[1405px] ml-50'>
+        <img src="images/bannerimage.png" alt="bannerimage" />
+        </div>
+    </div>
+  )
+}
+
+export default Banner
