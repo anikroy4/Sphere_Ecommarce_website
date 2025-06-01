75fc301c632b633fb5da956f9c9e73cd871cc642
Anik Roy <anikroy.uiu@gmail.com>
2025-06-01 22:40:37 +0600

    changes

--- src/components/searchpart/Search.jsx
+++ src/components/searchpart/Search.jsx
@@ -7,8 +7,14 @@ const Search = () => {
         <div className='pt-[62px] pb-[81.1px] ml-[73px]'>
             <h5 className='text-[#fff] font-["Prompt"] text-[46px] w-[473px] font-bold leading-[120%] tracking-[-0.92px] '>Gateway to a world of innovation with</h5>
             <img className=" mt-[17px]" src="/images/spherelogosearchbar.png" alt="" />
+        </div>
+
+        <div>
+          <div className='mr-[51px]'>
+            <input type="text" placeholder='Search for a product, category, or brand' className=' w-[591px] h-[60px] rounded-[30px] px-6 text-[16px] font-["Prompt"] font-normal bg-[#fff] border border-[#E0E0E0] focus:outline-none focus:border-[#000]' />
+          </div>
+
         </div> 
-        <div>Right</div> 
     </Container>
   )
 }
--- src/components/sections/footer/Footer.jsx
+++ src/components/sections/footer/Footer.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import Container from  "../../layers/Container"
+import List from '../../layers/List'
+import ListItem from '../../layers/ListItem'
+import { Link } from 'react-router-dom'
+import TelephoneIcon from '../../../icons/TelephoneIcon'
+import EmailIcon from '../../../icons/EmailIcon'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <Container className="flex  items-start justify-space justify-between mt-[73px] mb-[80px] ">
+        <div className='pt-[5px]' >
+          <img  src="/images/footericon.png" alt="footericon.png" />
+          <p className='mt-[57px] w-[381px] font-["Montserrat"] font-normal text-lg leading-[155%] text-[#000] '>Lorem ipsum dolor sit amet consectetur. Eleifend nec morbi tellus vitae leo nunc.</p>
+        </div>
+
+        
+        <div >
+          <h4 className='text-black font-["Montserrat"] text-[22px] !font-extrabold leading-[160%]  no-ligatures'>Company</h4>
+          <List className="">
+            <ListItem className="my-[13px] text-black font-['Montserrat'] text-lg font-normal leading-[160%] no-ligatures " ListInnerItem= "About Us"/>
+
+            <ListItem className="my-[13px] text-black font-['Montserrat'] text-lg font-normal leading-[160%] no-ligatures" ListInnerItem= "Products Digital"/>
+          
+            <ListItem className="text-black font-['Montserrat'] text-lg font-normal leading-[160%] no-ligatures" ListInnerItem= "Customer Review"/>
+          </List>
+
+        </div>
+
+        <div >
+          <h4 className='text-black font-["Montserrat"] text-[22px] !font-extrabold leading-[160%]  no-ligatures'>Information</h4>
+          <List className="">
+            <ListItem className="my-[13px] text-black font-['Montserrat'] text-lg font-normal leading-[160%] no-ligatures " ListInnerItem= "Help Center"/>
+   
+            <ListItem className=" text-black font-['Montserrat'] text-lg font-normal leading-[160%] no-ligatures" ListInnerItem= "Payment Methods"/>
+         
+            <ListItem className="my-[13px] text-black font-['Montserrat'] text-lg font-normal leading-[160%] no-ligatures" ListInnerItem= "Return & Refund"/>
+          </List>
+
+        </div>
+        
+        <div>
+          <h4 className='text-black font-["Montserrat"] text-[22px] !font-extrabold leading-[160%]  no-ligatures'>Contact</h4>
+          <List className="font-['Montserrat'] text-lg font-normal" >
+            <li className="my-[25px] items-center leading-[160%] ">
+              <Link to="tel:(+1) 123-456-7890">
+             <TelephoneIcon className=" inline-flex items-center mr-[10px]"/>(+1) 123-456-7890
+              </Link>
+            </li>
+            <li className=" leading-[160%] items-center">
+              <Link to="mailto:email@youremail.com">
+              <EmailIcon className=" inline-flex items-center  mr-[10px]"/>email@youremail.com
+              </Link>
+            </li>
+          </List>
+        </div>
+      </Container>
+    </>
   )
 }
 
